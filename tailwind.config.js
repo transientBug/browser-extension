6bb8a0d6d8edda464f56380a79d323e252c404d9
JoshAshby <joshuaashby@joshashby.com>
2019-06-18 11:14:48 -0600

    tweaks and fixes to tailwind for the macro
    the macro doesn't fully support tailwind 1 yet so the alpha has a few bugs
    especially around flex-grow
    
    This also configures the macro to allow using it like styled from emotion, backed by emotion
    I haven't figured out if emotions props in styled components works though

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+// module.exports = {
+//   theme: {
+//     extend: {}
+//   },
+//   variants: {},
+//   plugins: []
+// }
+
+// This is a work around for the macro not knowing flex-grow
+// https://github.com/bradlc/babel-plugin-tailwind-components/issues/32
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  ...defaultTheme,
   theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
+    flex: {
+      ...defaultTheme.flex,
+      grow: "1 0",
+      shrink: "0 1"
+    }
+  }
+};
