7bba06d2a5da6a696a5c505548f3f93264d85bc6
JoshAshby <joshuaashby@joshashby.com>
2019-06-18 11:17:27 -0600

    Add the navbar to the popup story view

--- src/stories/pages.js
+++ src/stories/pages.js
@@ -4,12 +4,14 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 // import { linkTo } from "@storybook/addon-links";
 
+import Navbar from "../components/Navbar";
 import PopupContainer from "../components/PopupContainer";
 
 import BookmarkEditView from "../pages/Popup/BookmarkEditView";
 
 storiesOf("Popup").add("BookmarkEditView", () => (
   <PopupContainer>
+    <Navbar />
     <BookmarkEditView
       bookmark={{}}
       autocompleteTags={["test"]}
