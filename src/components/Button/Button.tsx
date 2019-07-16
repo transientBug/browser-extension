70cc1923155a573d5c6ad906b1a57dd997940fae
JoshAshby <joshuaashby@joshashby.com>
2019-07-15 22:02:30 -0600

    add a "fitted button" which only expands to content width

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,13 @@
 import tw from "tailwind.macro";
+import styled from "@emotion/styled/macro";
 
 const Button = tw.button`
-  bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 my-1 border border-gray-400 rounded shadow
+  bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 my-1 border border-gray-400 rounded
+`;
+
+const FittedButton = styled(Button)`
+  width: max-content;
 `;
 
 export default Button;
+export { FittedButton };
